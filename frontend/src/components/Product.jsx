132ea42c1698db22144881bf97c4c25acec2f121
Erik Busch <erik_busch@gmx.de>
2022-09-04 16:49:52 +0200

    added responsive design

--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import './Product.css' 
 
 function Product({ product }) {
     return (
-        <div>
+        <div className="col-lg-3 col-md-6 col-sm-12 product-card" >
             <div>{product.ProductName}</div>
-            <div>{product.ProductPicture}</div>
+            <div>
+                <img className='product-photo' src={product.ProductPicture} alt='ProductPicture' /></div>
             <div>{product.ProductDescription}</div>
             <div>{product.ProductPrice}</div>
         </div>
--- frontend/src/components/ProductList.jsx
+++ frontend/src/components/ProductList.jsx
@@ -5,7 +5,8 @@ import Product from './Product'
 function ProductList({ products }) {
 
     return (
-        <div>
+        <div className="row">
+
             {products.map((product, index) => {
                 return (
                     <Product product={product} key={index} />
